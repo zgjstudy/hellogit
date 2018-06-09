27423ae2d5bd64faf550f4fc07ab76fbb0a8986f
zgjstudy <32426284+zgjstudy@users.noreply.github.com>
2018-06-09 10:13:11 +0800

    practice

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "|-----------|" << endl;
+    cout << "|Hello world|" << endl;
+    cout << "|-----------|" << endl;
+    return 0;
+}
